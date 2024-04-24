a15227789fae6475050077e9ec0d1a1bc9adaf49
Kolo <Kolo@DESKTOP-HLM3MBH>
2024-04-24 14:06:37 +0300

    First Commit

---
+++ TaskBoard.API/Controllers/CardsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskBoard.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardsController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Works";
+        }
+    }
+}
